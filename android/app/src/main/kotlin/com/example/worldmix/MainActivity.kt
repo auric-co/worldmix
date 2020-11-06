c683c5c99d93585077597acad9ffc888b73869b3
Christopher Nyandoro <54586967+devmaczw@users.noreply.github.com>
2020-11-06 13:19:30 +0200

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/worldmix/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.worldmix
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+import 'package:momentum/momentum.dart';
+import 'package:worldmix/src/services/client_db.dart';
+
+void main() {
+  runApp(momentum());
+}
+
+Momentum momentum() {
+  return Momentum(
+    key: UniqueKey(),
+    restartCallback: main,
+    controllers: [CounterController()],
+      child: MyApp(),
+    services: [
+      ClientDB(),
+    ],
+    persistSave: (context, key, value) async {
+      var sharedPref = await ClientDB.getByContext(context);
+      var result = await sharedPref.setString(key, value);
+      return result;
+    },
+    persistGet: (context, key) async {
+      var sharedPref = await ClientDB.getByContext(context);
+      var result = sharedPref.getString(key);
+      return result;
+    },
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Worldmix',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: HomeWidget(),
+    );
+  }
+}
+
+class CounterController extends MomentumController<CounterModel> {
+  @override
+  CounterModel init() {
+    return CounterModel(
+      this,
+      value: 0,
+    );
+  }
+
+  void increment() {
+    var value = model.value; // grab the current value
+    model.update(value: value + 1); // update state (rebuild widgets)
+    print(model.value); // new or updated value
+  }
+}
+
+class CounterModel extends MomentumModel<CounterController> {
+  CounterModel(
+    CounterController controller, {
+    this.value,
+  }) : super(controller);
+
+  final int value;
+
+  @override
+  void update({
+    int value,
+  }) {
+    CounterModel(
+      controller,
+      value: value ?? this.value,
+    ).updateMomentum();
+  }
+}
+
+class HomeWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Momentum Counter'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            MomentumBuilder(
+              controllers: [CounterController],
+              builder: (context, snapshot) {
+                var counter = snapshot<CounterModel>();
+                return Text(
+                  '${counter.value}',
+                  style: Theme.of(context).textTheme.headline4,
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+      // we don't need to rebuild the increment button, we can skip the MomentumBuilder
+      floatingActionButton: FloatingActionButton(
+        onPressed: Momentum.controller<CounterController>(context).increment,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
